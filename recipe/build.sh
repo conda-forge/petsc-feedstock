d2d0c2050e221b1d4724a2cb4f203a177f5a9b63
Lisandro Dalcin <dalcinl@gmail.com>
2017-10-02 09:48:07 +0300

    Update to version 3.8.0

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,14 +3,24 @@
 export PETSC_DIR=$SRC_DIR
 export PETSC_ARCH=arch-conda-c-opt
 
+unset CC
+unset CXX
+if [[ $(uname) == Linux ]]; then
+    export LDFLAGS="-pthread $LDFLAGS"
+fi
 
 $PYTHON ./configure \
+  CPPFLAGS="$CPPFLAGS" \
+  CFLAGS="$CFLAGS" \
+  CXXFLAGS="$CXXFLAGS" \
   LDFLAGS="$LDFLAGS" \
-  --with-fc=0 \
-  --with-debugging=0 \
   --COPTFLAGS=-O3 \
   --CXXOPTFLAGS=-O3 \
-  --LIBS=-Wl,-rpath,$PREFIX/lib \
+  --with-clib-autodetect=0 \
+  --with-cxxlib-autodetect=0 \
+  --with-fortranlib-autodetect=0 \
+  --with-fc=0 \
+  --with-debugging=0 \
   --with-blas-lapack-lib=libopenblas${SHLIB_EXT} \
   --with-hwloc=0 \
   --with-mpi=1 \
@@ -28,9 +38,9 @@ done
 
 make
 
+# FIXME: make check prevents upload on CircleCI
+# See https://github.com/conda-forge/conda-smithy/pull/337
 if [[ $(uname) == Darwin ]]; then
-    # FIXME: make check prevents upload on CircleCI
-    # See https://github.com/conda-forge/conda-smithy/pull/337
     make check
 fi
 
@@ -39,7 +49,10 @@ make install
 rm -fr $PREFIX/bin && mkdir $PREFIX/bin
 rm -fr $PREFIX/share && mkdir $PREFIX/share
 rm -fr $PREFIX/lib/lib$PKG_NAME.*.dylib.dSYM
+rm -f  $PREFIX/lib/$PKG_NAME/conf/.DIR
+rm -f  $PREFIX/lib/$PKG_NAME/conf/mpitest.c
 rm -f  $PREFIX/lib/$PKG_NAME/conf/files
+rm -f  $PREFIX/lib/$PKG_NAME/conf/testfiles
 rm -f  $PREFIX/lib/$PKG_NAME/conf/*.py
 rm -f  $PREFIX/lib/$PKG_NAME/conf/*.log
 rm -f  $PREFIX/lib/$PKG_NAME/conf/RDict.db
--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -1,11 +1,12 @@
+#!/bin/bash
 set -e
 
 export PETSC_DIR=${PREFIX}
 cd "src/snes/examples/tests"
 make ex1
 
-# FIXME: runex1 causes early termination with success (?!) on Linux
-# preventing upload. Still a complete mystery.
-if [[ "$(uname)" == "Darwin" ]]; then
+# FIXME: make check prevents upload on CircleCI
+# See https://github.com/conda-forge/conda-smithy/pull/337
+if [[ $(uname) == Darwin ]]; then
     make runex1
 fi
