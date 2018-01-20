84192b7de654e92ef38678584b4437d7b3962bd2
Lisandro Dalcin <dalcinl@gmail.com>
2018-01-20 15:43:47 +0300

    Issue 24: Enable Fortran support

--- recipe/mpiexec.sh
+++ recipe/mpiexec.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 # pipe stdout, stderr through cat to avoid O_NONBLOCK issues
-mpiexec $@ 2>&1</dev/null | cat
+mpiexec -launcher fork $@ 2>&1 </dev/null | cat
--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -4,8 +4,10 @@ set -e
 export PETSC_DIR=${PREFIX}
 cd "src/snes/examples/tests"
 make ex1
+make ex1f
 
 # FIXME: Workaround mpiexec setting O_NONBLOCK in std{in|out|err}
 # See https://github.com/conda-forge/conda-smithy/pull/337
 # See https://github.com/pmodels/mpich/pull/2755
-make runex1 MPIEXEC="${RECIPE_DIR}/mpiexec.sh"
+make runex1  MPIEXEC="${RECIPE_DIR}/mpiexec.sh"
+make runex1f MPIEXEC="${RECIPE_DIR}/mpiexec.sh"
