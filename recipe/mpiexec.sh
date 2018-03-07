3952d03ecb5785ae01fad37cff95af8c309244f2
Lisandro Dalcin <dalcinl@gmail.com>
2018-03-07 18:05:11 +0300

    Add MPI variants

--- recipe/mpiexec.sh
+++ recipe/mpiexec.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 set -e
-# pipe stdout, stderr through cat to avoid O_NONBLOCK issues
-mpiexec -launcher fork $@ 2>&1 </dev/null | cat
+
+if [[ "$MPI_VARIANT" == "mpich" ]]; then
+    export HYDRA_LAUNCHER=fork
+elif [[ "$MPI_VARIANT" == "openmpi" ]]; then
+    export OMPI_MCA_plm=isolated
+    export OMPI_MCA_rmaps_base_oversubscribe=yes
+    export OMPI_MCA_btl_vader_single_copy_mechanism=none
+    mpiexec_args=--allow-run-as-root
+fi
+
+mpiexec $mpiexec_args $@ 2>&1 </dev/null | cat
