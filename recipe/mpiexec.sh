018fc9bf3379f4b9ea321c1c81983eea2943bcb3
Lisandro Dalcin <dalcinl@gmail.com>
2018-06-17 00:54:55 +0300

    Update mpiexec.sh wrapper script to support Open MPI in the near future

--- recipe/mpiexec.sh
+++ recipe/mpiexec.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
-set -e
+set -euo pipefail
+
+if command -v "${PREFIX}/bin/mpichversion" >/dev/null; then
+    export HYDRA_LAUNCHER=fork
+    mpiexec="mpiexec"
+fi
+
+if command -v "${PREFIX}/bin/ompi_info" >/dev/null; then
+    export OMPI_MCA_plm=isolated
+    export OMPI_MCA_rmaps_base_oversubscribe=yes
+    export OMPI_MCA_btl_vader_single_copy_mechanism=none
+    mpiexec="mpiexec --allow-run-as-root"
+fi
+
 # pipe stdout, stderr through cat to avoid O_NONBLOCK issues
-mpiexec -launcher fork $@ 2>&1 </dev/null | cat
+$mpiexec $@ 2>&1 | cat
