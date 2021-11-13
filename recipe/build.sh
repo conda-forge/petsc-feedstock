25fee2d65f12e8acc4a34f9e15bd2e503f0cfe65
Jan Janssen <jan-janssen@users.noreply.github.com>
2021-11-13 10:32:20 -0700

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -31,7 +31,7 @@ if [[ $mpi == "openmpi" ]]; then
   export OMPI_MCA_plm=isolated
   export OMPI_MCA_rmaps_base_oversubscribe=yes
   export OMPI_MCA_btl_vader_single_copy_mechanism=none
-  export WITH_SUPERLU_DIST=0
+  export WITH_SUPERLU_DIST=1
 elif [[ $mpi == "mpich" ]]; then
   export HYDRA_LAUNCHER=fork
   export WITH_SUPERLU_DIST=1
