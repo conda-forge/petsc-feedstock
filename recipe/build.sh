350e6ba476e6d3c36b48994085ea2f2e95627bfc
Min RK <benjaminrk@gmail.com>
2025-04-01 15:28:09 +0200

    maybe we don't need to scrub some compiler flags now that we're using OPTFLAGS

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,25 +7,6 @@ cp $BUILD_PREFIX/share/gnuconfig/config.* .
 export PETSC_DIR=$SRC_DIR
 export PETSC_ARCH=arch-conda-c-opt
 
-unset FC
-unset F90
-unset F77
-unset CC
-unset CXX
-
-if [[ "$target_platform" == linux-* ]]; then
-    export LDFLAGS="$LDFLAGS -Wl,-rpath-link,$PREFIX/lib"
-    # --as-needed appears to cause problems with fortran compiler detection
-    # due to missing libquadmath
-    # unclear why required libs are stripped but still linked
-    export FFLAGS="${FFLAGS:-} -Wl,--no-as-needed"
-fi
-
-# scrub debug-prefix-map args, which cause problems in pkg-config
-CFLAGS=$(echo ${CFLAGS:-} | sed -E 's@\-fdebug\-prefix\-map[^ ]*@@g')
-CXXFLAGS=$(echo ${CXXFLAGS:-} | sed -E 's@\-fdebug\-prefix\-map[^ ]*@@g')
-FFLAGS=$(echo ${FFLAGS:-} | sed -E 's@\-fdebug\-prefix\-map[^ ]*@@g')
-
 if [[ $mpi == "openmpi" ]]; then
   LIBS="-Wl,-rpath,$PREFIX/lib -lmpi_mpifh -lgfortran"
 elif [[ $mpi == "mpich" ]]; then
@@ -81,7 +62,8 @@ fi
 
 # unexport the variables to reduce warnings about config we know isn't used
 # (This doesn't unset variables, just prevents the export for subprocesses)
-export -n AR CFLAGS CXXFLAGS CPPFLAGS LDFLAGS
+export -n AR FC F90 F77 CC CXX
+export -n CFLAGS CXXFLAGS CPPFLAGS LDFLAGS
 
 # petsc doesn't want us to set CFLAGS, etc.
 # pass compiler flags via {C,CXX,F}OPTFLAGS to extend defaults
